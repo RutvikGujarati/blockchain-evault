4acdd23b6c6269e604530b7e92c543538b23dcb6
keyurpatel29 <143800818+keyurpatel29@users.noreply.github.com>
2023-09-16 14:06:51 +0530

    Update Navbar.jsx

--- evault/src/Navbar/Navbar.jsx
+++ evault/src/Navbar/Navbar.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { ConnectWallet } from "@thirdweb-dev/react";
-
 import "./Navbar.css";
+import logo from '../images/logo.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouseChimney, faAward , faIdCard, faCircleInfo, faLightbulb} from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar">
-        <div className="Home">
-          <Link to="/home">Home</Link>
+    <div className="Main-header">
+      <div className="header">
+        <div className="logo-1">
+          <img src={logo} alt="Logo" />
         </div>
-        <div className="About">
-          <Link to="/about">About</Link>
-        </div>
-        <div className="Vault">
-          <Link to="/vault">Vault</Link>
-        </div>
+
         <div className="connect">
           <ConnectWallet
             dropdownPosition={{
@@ -25,6 +22,37 @@ const Navbar = () => {
             }}
           />
         </div>
+      </div>
+      <nav className="navbar">
+
+        <div className="Home">
+          <a href="/home">
+            <i><FontAwesomeIcon icon={faHouseChimney} style={{ color: "#E0FFFF" }} /></i> Home
+          </a>
+        </div>
+
+        <div className="Get Document">
+          <a href="/getdocument">
+            <i><FontAwesomeIcon icon={faIdCard} style={{color: "#E0FFFF",}} /></i>Get Document
+            </a>
+        </div>
+
+        <div className="Issued document">
+          <a href="/issueddocument">
+            <i><FontAwesomeIcon icon={faAward} style={{color: "#E0FFFF",}} /></i>Issued Document
+          </a>
+        </div>
+
+        <div className="About">
+          <a href="/About">
+            <i><FontAwesomeIcon icon={faCircleInfo} style={{color: "#E0FFFF",}} /></i>About</a>
+        </div>
+
+        <div className="Help">
+          <a href="/help">
+            <i><FontAwesomeIcon icon={faLightbulb} style={{color: "#E0FFFF",}} /></i>Help</a>
+        </div>
+
       </nav>
     </div>
   );
